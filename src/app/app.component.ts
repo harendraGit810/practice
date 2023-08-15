0eab980aed7f27257f399573289b3489701b0430
harendragit810 <harendramail810@gmail.com>
2023-08-16 02:12:33 +0530

    learned the directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+
+  data = [ "Harendra", "Devendra","Munni", "Narendra", "Seema"];
+
+  dataObject = [
+    {"id":1 , "name":"Harendra", "age":31 , "status": "present"},
+    {"id":2 , "name":"Narendra", "age":33, "status": "absent"},
+    {"id":3 , "name":"Seema", "age":35, "status": "absent"},
+    {"id":4 , "name":"Devendra", "age":62 , "status": "present"},
+    {"id":5 , "name":"Munni", "age":60, "status": "absent"},
+    {"id":6 , "name":"Pratima", "age":32, "status": "present"}
+  ]
+
+  searchValue : string = "";
 }
