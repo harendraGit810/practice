1ae0e2af67d060320ed67720c8c76abedd769d53
Harendra Singh <harendramail810@gmail.com>
2023-08-25 01:22:28 +0530

    build

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -9,4 +9,15 @@ export class ChildComponent {
  @Input() total:number = 0 ;
 
  newProp:string="";
+ selectedValue:any;
+ records = [
+  {"id":1 , "name":"Harendra", "age":31 , "status": "present"},
+  {"id":2 , "name":"Narendra", "age":33, "status": "absent"},
+  {"id":3 , "name":"Seema", "age":35, "status": "absent"},
+  {"id":4 , "name":"Devendra", "age":62 , "status": "present"},
+  {"id":5 , "name":"Munni", "age":60, "status": "absent"},
+  {"id":6 , "name":"Pratima", "age":32, "status": "present"}
+]
+
+
 }
